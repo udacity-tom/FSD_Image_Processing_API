9fd40f1012d744922385dde1b6518e15c60ee48f
udacity-tom <udacity@iamtom.com>
2021-06-29 10:54:20 +0200

    Feat: generalising sharp implementation

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ const port = 3000;
 
 //initial middlware stub
 function convertImage() {
-  console.log(`Àn image will be converted`);
-  const fileName: string = 'fjord.jpg';
-  sharpUtil(fileName);
+    const xValue = 300;
+    const yValue = 200;
+    const imagePath: string = __dirname+'/convert/';
+    console.log(`Àn image will be converted`);
+    const fileName: string = 'fjord.jpg';
+    console.log('fileName imagePath', fileName, imagePath);
+    sharpUtil(fileName, imagePath, xValue, yValue,);
 }
 
 app.get('/convertImage', (req, res) => {
-  convertImage();
-  res.send('Request received');
+    convertImage();
+    res.send('Request received');
 });
 
 app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+    console.log(`Server started at http://localhost:${port}`);
 });
 
 
--- src/utilities/sharpUtil.ts
+++ src/utilities/sharpUtil.ts
@@ -2,27 +2,10 @@ import sharp  from 'sharp';
 
 // const sharp = 
 
-const sharpApp: any = sharp();
+// const sharpApp: any = sharp();
 
-
-// function sharpUtil(filename: string): string {
-    // let sharpUtil = await sharp({
-    //     create: {
-    //     width: 48,
-    //     height: 48,
-    //     channels: 4,
-    //     background: { r: 255, g: 0, b: 0, alpha: 0.5 }
-    //     }
-    // })
-    //     .png()
-        // .toBuffer()
-        // .toFile('output.jpg', function(err)  {
-        //     console.log('sharp error is', err);
-        // })
-// }
-
-function sharpUtil(fileName: string): void {
-    sharp('fjord.jpg')
+function sharpUtil(fileName: string, imagePath: string, xValue: number, yValue: number): void {
+    sharp(imagePath+fileName)
     // sharp({
     //     create: {
     //     width: 48,
@@ -30,8 +13,8 @@ function sharpUtil(fileName: string): void {
     //     channels: 4,
     //     background: { r: 255, g: 0, b: 0, alpha: 0.5 } 
     // }})
-    .resize(300, 200)
-    .toFile('output.jpg', function(err, info) {
+    .resize(xValue, yValue)
+    .toFile(imagePath+'output.jpg', function(err, info) {
         console.log('sharp error', err, info);
         // console.log('current path', __dirname);
         // output.jpg is a 300 pixels wide and 200 pixels high image
