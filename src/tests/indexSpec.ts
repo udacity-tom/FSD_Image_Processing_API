9fffa6b23f03b4af437361997b629bdf13c07ac6
udacity-tom <udacity@iamtom.com>
2021-06-29 08:43:40 +0200

    Chore: added another Jasmine test

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -4,10 +4,20 @@ import app from  '../index';
 
 const request = supertest(app);
 
-describe('Tests endpoint', () => {
+describe('Tests endpoint is responsive', () => {
     it('checks response from the endpoint', async (done) => {
         const response = await request.get('/convertImage');
         expect(response.status).toBe(200);
         done();
-    })
+    });
+describe('Tests current setup response of endpoint', () => {
+    it('checks response is the current text', async (done) => {
+        const response = await request.get('/convertImage');
+        // console.log('response',response);
+        expect(response.text).toBe('Request received');
+        done();
+    });
+})
+
+
 });
