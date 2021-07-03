dd30c35838eb32895e88ca4eec715bd58b04b23a
udacity-tom <udacity@iamtom.com>
2021-07-03 19:36:53 +0200

    Refactor: rewrote Util function for img resize

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,77 @@
 import express from 'express';
-import sharpUtil from './utilities/sharpUtil';
+import sharpConvert from './utilities/sharpUtil';
 // import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 //initial middlware stub
-function convertImage(convert:{filename:string, width:number, height:number}): string {
-    const imagePath: string = __dirname.slice(0,__dirname.length-4)+'/assets/';
-    // console.log('imagePath',imagePath);
-    console.log(`The file named ${convert.filename} will be converted to ${convert.width} by ${convert.height}`);
-    return sharpUtil(convert.filename, imagePath, convert.width, convert.height);
 
-}
 
-app.get('/convertImage', (req, res) => {
+ app.get('/convertImage', async (req, res) => {
     console.log('req.query', req.query);
-    console.log('type of req.query', typeof req.query);
+    // console.log('type of req.query', typeof req.query);
     let convert:{filename:string, width:number, height:number} = req.query as unknown as {filename:string, width:number, height:number};
-
+    const filename = req.query.filename as unknown as string;
+    const imagePath: string = process.cwd()+'/assets/';
+    const inputFile: string = `${imagePath}full/${filename}`;
+    const outputFile: string = `${imagePath}thumb/${convert.filename.slice(0,convert.filename.length-4)}_${convert.width}_${convert.height}.jpg`;
+    const width: number = req.query.width as unknown as number;
+    const height: number = req.query.height as unknown as number;
+    let fileToConvert:{inputFile:string, outputFile:string, width:number, height:number} = {
+        inputFile,
+        outputFile,
+        width,
+        height
+    }
+    
     //TODO: read files in thumb directory, check if present with given parameters, then serve 'cached' file from disk
     //use node fs.read (file test criteria `${imagePath}thumb/${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`)
     // convertImage(convert);
     // res.send('Request received');
-    const pathOfImage: string = convertImage(convert);
-    console.log('convertImage()', );
-    res.sendFile(convertImage(convert));
+    // const pathOfImage: string = convertImage(convert);
+    // console.log('convertImage()', );
+
+    // const convertedImage: Promise<string> = (async (): Promise<string> => {
+    //      return await convertImage(convert) as unknown as string ;
+    // })();
+
+
+    // let responsePath:string; 
+
+    // const convertedImage: string = sharpConvert(convert) as unknown as string;
+    // const convertedImage = async () => {
+    //     const response =  sharpConvert(convert);
+    //     console.log('index.ts response is', await Promise.resolve(response));
+    //     let responsePath = await Promise.resolve(response) ;
+    //     return responsePath;
+    // }
+    
+    // let convertedImage = sharpConvert(convert);
+    // console.log('convertedImage index.ts',convertedImage);
+    
+    // convertedImage();
+    // console.log("convertedImage", convertedImage());
+    // let responsePath = convertedImage() as unknown as string;
+    // console.log('responsePath', responsePath);
+    // res.sendFile(responsePath);
+    
+    // process.nextTick( () => res.sendFile(convertedImage() as unknown as string));
+    
+    // console.log('sharpConvert',sharpConvert(convert));
+    
+    // res.sendFile(responsePath);
+    // console.log('sharpConvert(fileToConvert)',sharpConvert(fileToConvert));
+
+    await sharpConvert(fileToConvert);
+    
+    res.sendFile(outputFile);
+
+    // res.sendFile(sharpConvert(convert) as unknown as string);
+    
+    // .catch((err: any) => {
+    //     res.send("There was an error", err);
+    // })
 });
 
 app.listen(port, () => {
--- src/utilities/sharpUtil.ts
+++ src/utilities/sharpUtil.ts
@@ -1,64 +1,24 @@
-import sharp  from 'sharp';
+// import sharp, { Sharp }  from 'sharp';
+import sharp from 'sharp';
+import {promises as fsPromises} from 'fs';
+import { resolve } from 'path';
 
-// const sharp = 
+const sharpConvert = async (convert:{inputFile:string, outputFile:string, width:number, height:number}):Promise<string | void> => {
+    let response = await sharp(convert.inputFile)
+    .resize(Number(convert.width), Number(convert.height))
+    .toBuffer()
+    .then(  async data => {
+        console.log('in then clause');
+        await fsPromises.writeFile(convert.outputFile, data);
+        console.log('after fsPromises.writeFiel');
+    });
 
-// const sharpApp: any = sharp();
+    // return response;
+    await Promise.resolve(response);
+    console.log('promise resolved');
+        return  convert.outputFile;
+    // }
+// return await Promise.resolve(response);
+}
 
-
-
-
-// const sharpUtil = async (fileName: string, imagePath: string, xValue: number, yValue: number): boolean => {
-//     sharp(imagePath+fileName)
-//     // sharp({
-//     //     create: {
-//     //     width: 48,
-//     //     height: 48,
-//     //     channels: 4,
-//     //     background: { r: 255, g: 0, b: 0, alpha: 0.5 } 
-//     // }})
-//     .resize(xValue, yValue)
-//     .toFile(`${imagePath}${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`, function(err) {
-//         if(err != null){
-//             console.log('sharp error', err)
-//             return false;
-//         };
-//         // console.log('current path', __dirname);
-//         // output.jpg is a 300 pixels wide and 200 pixels high image
-//         // containing a scaled and cropped version of input.jpg
-//         return true;
-//     });
-// }
-
-function sharpUtil(fileName: string, imagePath: string, xValue: number, yValue: number): string {
-    
-        sharp(imagePath+'full/'+fileName)
-        .resize(Number(xValue), Number(yValue))
-        .toFile(`${imagePath}thumb/${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`, function(err) {
-            if(err != null){
-                console.log('sharp error', err)
-                // return false;
-            };
-            console.log('image',`${imagePath}thumb/${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`);
-        });
-        return `${imagePath}thumb/${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`;
-    }
-
-  export default sharpUtil;
-// function sharpUtil(fileName: string, imagePath: string, xValue: number, yValue: number): string {
-    
-//     const answer: string = ((function (imagePath,fileName): string {
-//         sharp(imagePath+'full/'+fileName)
-//         .resize(Number(xValue), Number(yValue))
-//         .toFile(`${imagePath}thumb/${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`, function(err) {
-//             if(err != null){
-//                 console.log('sharp error', err)
-//                 // return false;
-//             };
-//             console.log('image',`${imagePath}thumb/${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`);
-            
-//         });
-//         return `${imagePath}thumb/${fileName.slice(0,fileName.length-4)}_${xValue}_${yValue}.jpg`;
-//     }))(imagePath, fileName);
-//     return answer;
-// }
-//   export default sharpUtil;
+export default sharpConvert;
