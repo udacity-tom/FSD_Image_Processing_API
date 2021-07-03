1b8d622c62e70d0850aede94c54d7a9bd0800123
udacity-tom <udacity@iamtom.com>
2021-07-03 19:55:41 +0200

    Refactor: code refactoring, removed dead code

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
 import express from 'express';
 import fs from 'fs';
 import sharpConvert from './utilities/sharpUtil';
-// import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-//initial middlware stub
-
-
  app.get('/convertImage', async (req, res) => {
-    console.log('req.query', req.query);
-    // console.log('type of req.query', typeof req.query);
-    let convert:{filename:string, width:number, height:number} = req.query as unknown as {filename:string, width:number, height:number};
     const filename = req.query.filename as unknown as string;
     const imagePath: string = process.cwd()+'/assets/';
-    const inputFile: string = `${imagePath}full/${filename}`;
-    const outputFile: string = `${imagePath}thumb/${convert.filename.slice(0,convert.filename.length-4)}_${convert.width}_${convert.height}.jpg`;
     const width: number = req.query.width as unknown as number;
     const height: number = req.query.height as unknown as number;
+    const inputFile: string = `${imagePath}full/${filename}`;
+    const outputFile: string = `${imagePath}thumb/${filename.slice(0,filename.length-4)}_${width}_${height}.jpg`;
     let fileToConvert:{inputFile:string, outputFile:string, width:number, height:number} = {
         inputFile,
         outputFile,
@@ -32,15 +25,7 @@ const port = process.env.PORT || 3001;
     } else {
         console.log("File already exists, serving from disk");
     }
-    
-    
     res.sendFile(outputFile);
-
-    // res.sendFile(sharpConvert(convert) as unknown as string);
-    
-    // .catch((err: any) => {
-    //     res.send("There was an error", err);
-    // })
 });
 
 app.listen(port, () => {
--- src/utilities/sharpUtil.ts
+++ src/utilities/sharpUtil.ts
@@ -4,21 +4,14 @@ import {promises as fsPromises} from 'fs';
 import { resolve } from 'path';
 
 const sharpConvert = async (convert:{inputFile:string, outputFile:string, width:number, height:number}):Promise<string | void> => {
-    let response = await sharp(convert.inputFile)
+    await sharp(convert.inputFile)
     .resize(Number(convert.width), Number(convert.height))
     .toBuffer()
-    .then(  async data => {
-        console.log('in then clause');
+    .then(  
+        async data => {
         await fsPromises.writeFile(convert.outputFile, data);
-        console.log('after fsPromises.writeFiel');
     });
-
-    // return response;
-    await Promise.resolve(response);
-    console.log('promise resolved');
         return  convert.outputFile;
-    // }
-// return await Promise.resolve(response);
 }
 
 export default sharpConvert;
