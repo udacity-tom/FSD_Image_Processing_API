19b053bcd21d7d1e8b20e1d822ab816e86ae7a0b
udacity-tom <udacity@iamtom.com>
2021-07-09 17:33:12 +0200

    Chore: tidy up, comments

--- src/routes/api/convert.ts
+++ src/routes/api/convert.ts
@@ -6,17 +6,14 @@ import imageCheck from '../../utilities/imageCheck';
 
 const convert = express.Router();
 
-// convert.get('/convert', )
 //async endpoint to convert image, Middleware to handle errors and log transactions to console.
 convert.get('/',defaultPage, errHandler, async (req: { query: object; }, res: { sendFile: (arg: string ) => void; }) => {
    let fileObj = getFileDetails(req.query) as unknown as {filename: string, fileExtension: string, outputFilename: string, fileOutputExt: string, inputFile: string, outputFile: string, width: number, height: number};
-   // if(fileObj.fileOutputExt == undefined)fileObj.fileOutputExt = fileObj.fileExtension;
-   //Checks if image is on disk/otherwise creates the file.
    await imageCheck(fileObj);
    res.sendFile(fileObj.outputFile);
 
 });
-
+//returns instructions page if no parameters passed to /convert
 function defaultPage(req: {query: any; path: any;}, res: any, next: () => void) {
     if(Object.keys(req.query).length == 0){
         res.redirect('/');
--- src/utilities/imageCheck.ts
+++ src/utilities/imageCheck.ts
@@ -2,11 +2,12 @@ import sharpConvert from './sharpUtil';
 import fs from 'fs';
 import getTimeAndDate from './getTimeAndDate';
 //checks if image is already on disk
-async function imageCheck (giveObj: object) {
-    let obj = giveObj as unknown as {filename: string, fileExtension: string, outputFilename: string, fileOutputExt: string, inputFile: string, outputFile: string, width: number, height: number};
+async function imageCheck (givenObj: object) {
+    let obj = givenObj as unknown as {filename: string, fileExtension: string, outputFilename: string, fileOutputExt: string, inputFile: string, outputFile: string, width: number, height: number};
     //fs.existsSync returns boolean for user requested file whether on disk
     if(!fs.existsSync(obj.outputFile)){
         console.log(`${getTimeAndDate()} ${obj.outputFilename} doesn't exist, creating it...`);
+        //resizes/reformats image if needed
         await sharpConvert(obj);
     } else {
         console.log(`${getTimeAndDate()} ${obj.outputFilename} already exists, serving from disk`);
